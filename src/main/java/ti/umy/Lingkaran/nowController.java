09272e041dc31bcc05273e771f6866a30257fe32
ffadelm <faysalfadelm0107@gmail.com>
2022-02-28 16:52:17 +0700

    menambahkan index html

---
+++ src/main/java/ti/umy/Lingkaran/nowController.java
@@ -0,0 +1,29 @@
+package ti.umy.Lingkaran;
+
+import java.text.DecimalFormat;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class nowController {
+    @RequestMapping("/hasil")
+    public String getHasil(Model model, HttpServletRequest param){
+        String jari = param.getParameter("jari");
+        double phi = 3.14;
+        double luas, keliling;
+        double r = Double.parseDouble(jari);
+        
+        luas = 0.5 * phi * r * r;
+        keliling = 2 * phi * r;
+        
+        DecimalFormat df = new DecimalFormat("#.##");
+        
+        model.addAttribute("Jari", jari);
+        model.addAttribute("Luas", luas);
+        model.addAttribute("Keliling", df.format(keliling));
+        
+        return "index";
+    }
+}
