51bc11f4698ec4040824b0591bddba39947a983f
ffadelm <faysalfadelm0107@gmail.com>
2022-02-28 16:42:08 +0700

    first commit

---
+++ src/main/java/ti/umy/Lingkaran/LingkaranApplication.java
@@ -0,0 +1,13 @@
+package ti.umy.Lingkaran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LingkaranApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LingkaranApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ti/umy/Lingkaran/LingkaranApplicationTests.java
@@ -0,0 +1,13 @@
+package ti.umy.Lingkaran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingkaranApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
